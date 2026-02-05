fd722b1a4ec587c5b134ad55ba395be16a2a10d8
Thato M <thatomsekele@gmail.com>
2026-02-05 23:31:50 +0200

    initial change

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,5 @@
 import random
-
+#List of the characters needed to create our password, including symbols
 password_characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+?><"
+
+password = " "
